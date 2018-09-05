a46c9ac7f52e86c17e675364b28dd39e347d3d55
FrranzMaurer <38557647+FrranzMaurer@users.noreply.github.com>
2018-09-05 17:48:20 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -26,40 +26,12 @@ var Counter = React.createClass({
     },
 });
 
-var Counter2 = React.createClass({
-    getInitialState: function() {
-        return {
-            counter: 0
-        };
-    },
-
-    increment: function() {
-        this.setState({
-            counter: this.state.counter + 1
-        });
-    },
-
-    decrement: function() {
-        this.setState({
-            counter: this.state.counter - 1
-        });
-    },
-
-    render: function() {
-        return React.createElement('div', {className: 'licznik2'},
-        	React.createElement('button', {onClick: this.increment}, 'Increase'),
-        	React.createElement('button', {onClick: this.decrement}, 'Decrease'),
-            React.createElement('span', {}, 'Licznik ' + this.state.counter)
-        );
-    },
-});
-
 var App = React.createClass({
   render: function() {
     return (
       React.createElement('div', {className: 'app'},
         React.createElement(Counter, {}),
-        React.createElement(Counter2, {}, {})
+        React.createElement(Counter, {})
       )
     );
   }
